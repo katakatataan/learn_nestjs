53a72c81ee734444a5538224a37dd4812c890e59
katakatataan <matsuyay@matsuyamayuukinoMacBook-Air.local>
2022-10-19 18:38:55 +0900

    update

---
+++ a0003_union/src/index.ts
@@ -0,0 +1,69 @@
+import {ApolloServer} from '@apollo/server';
+import {startStandaloneServer} from '@apollo/server/standalone';
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+union SearchResult = Book | Author
+
+type Book {
+  title: String!
+}
+
+type Author {
+  name: String!
+}
+
+type Query {
+  search(contains: String): [SearchResult!]
+}
+`;
+
+const resolvers = {
+  SearchResult: {
+    __resolveType(obj, context, info){
+      // Only Author has a name field
+      if(obj.name){
+        return 'Author';
+      }
+      // Only Book has a title field
+      if(obj.title){
+        return 'Book';
+      }
+      return null; // GraphQLError is thrown
+    },
+  },
+  Query: {
+    search: () => {
+      return [
+        {
+          "title": "helloo",
+
+        },
+        {
+          "name": "sample",
+        }
+      ]
+     }
+  },
+};
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+// Passing an ApolloServer instance to the `startStandaloneServer` function:
+//  1. creates an Express app
+//  2. installs your ApolloServer instance as middleware
+//  3. prepares your app to handle incoming requests
+const {url} = await startStandaloneServer(server, {
+  listen: {port: 4000},
+});
+
+console.log(`🚀  Server ready at: ${url}`);
